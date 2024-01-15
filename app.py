931de707701cfa35a2125fed7d8b48a1f8741ba6
xiaobai <319331998@qq.com>
2024-01-15 18:50:34 +0800

    [FEA] add calcu_advanced which can interacts with sqlite

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String, Float
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///calculations.db"
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# db = SQLAlchemy(app)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+class Calculation(db.Model):
+    id: Mapped[int] = db.Column(Integer, primary_key=True)
+    expression: Mapped[str] = db.Column(String(255), nullable=False)
+    result: Mapped[str] = db.Column(Float, nullable=False)
+
+    def __repr__(self):
+        return f"<Calculation {self.expression} = {self.result}>"
+
+
+with app.app_context():
+    db.create_all()
 
 # init config
 if os.getenv("APP_ENV") == "prod":
@@ -23,5 +50,18 @@ def calculator():
     return render_template("calculator.html")
 
 
+@app.route("/calculator_advanced", methods=["GET", "POST"])
+def calcu_advanced():
+    if request.method == "POST":
+        expression = request.form.get("expression")
+        result = eval(expression)  # Caution: Using eval() like this can be dangerous.
+        new_calculation = Calculation(expression=expression, result=result)
+        db.session.add(new_calculation)
+        db.session.commit()
+        return redirect(url_for("calcu_advanced"))
+    calculations = db.session.execute(db.select(Calculation)).scalars()
+    return render_template("calcu_advanced.html", calculations=calculations)
+
+
 if __name__ == "__main__":
-    app.run(host = host, port=8080)
+    app.run(host=host, port=8080)
